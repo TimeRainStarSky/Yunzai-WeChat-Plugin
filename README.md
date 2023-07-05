32e61b499862c03661a03547a2e3802a6b4a3578
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-07-05 16:06:14 +0800

    初版发布啦！

---
+++ README.md
@@ -0,0 +1,36 @@
+<div align="center">
+
+# TRSS-Yunzai WeChat Plugin
+
+TRSS-Yunzai 微信 Bot 适配器 插件
+
+[![访问量](https://visitor-badge.glitch.me/badge?page_id=TimeRainStarSky.Yunzai-WeChat-Plugin&right_color=red&left_text=访%20问%20量)](https://github.com/TimeRainStarSky/Yunzai-WeChat-Plugin)
+[![Stars](https://img.shields.io/github/stars/TimeRainStarSky/Yunzai-WeChat-Plugin?color=yellow&label=收藏)](../../stargazers)
+[![Downloads](https://img.shields.io/github/downloads/TimeRainStarSky/Yunzai-WeChat-Plugin/total?color=blue&label=下载)](../../archive/main.tar.gz)
+[![Releases](https://img.shields.io/github/v/release/TimeRainStarSky/Yunzai-WeChat-Plugin?color=green&label=发行版)](../../releases/latest)
+
+[![访问量](https://profile-counter.glitch.me/TimeRainStarSky-Yunzai-WeChat-Plugin/count.svg)](https://github.com/TimeRainStarSky/Yunzai-WeChat-Plugin)
+
+</div>
+
+## 安装教程
+
+1. 准备：[TRSS-Yunzai](../../../Yunzai)
+
+2. 自动安装：`#安装WeChat-Plugin`
+- 手动安装：[GitHub](https://github.com/TimeRainStarSky/Yunzai-WeChat-Plugin) 或 [Gitee](https://gitee.com/TimeRainStarSky/Yunzai-WeChat-Plugin)
+
+```
+git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-WeChat-Plugin plugins/WeChat-Plugin
+pnpm i
+```
+
+## 使用教程
+
+- #微信账号
+- #微信登录
+- #微信删除 + `账号`
+
+- 注意：
+1. 账号离线5分钟，即登录失效，需要重新登录
+2. 重新登录后，所有用户 ID 都会改变
---
+++ index.js
@@ -0,0 +1,513 @@
+logger.info(logger.yellow("- 正在加载 微信 插件"))
+
+import { config, configSave } from "./Model/config.js"
+import fetch from "node-fetch"
+import fs from "node:fs"
+import path from "node:path"
+import { fileTypeFromBuffer } from "file-type"
+import Wechat from "wechat4u"
+
+const adapter = new class WeChatAdapter {
+  constructor() {
+    this.id = "WeChat"
+    this.name = "微信Bot"
+    this.path = "data/WeChat/"
+    if (!fs.existsSync(this.path))
+      fs.mkdirSync(this.path)
+  }
+
+  makeParams(data) {
+    const params = []
+    for (const i of Object.keys(data))
+      params.push(`${encodeURIComponent(i)}=${encodeURIComponent(data[i])}`)
+    return `?${params.join("&")}`
+  }
+
+  async makeBuffer(file) {
+    if (file.match(/^base64:\/\//))
+      return Buffer.from(file.replace(/^base64:\/\//, ""), "base64")
+    else if (file.match(/^https?:\/\//))
+      return Buffer.from(await (await fetch(file)).arrayBuffer())
+    return file
+  }
+
+  async fileType(data) {
+    const file = {}
+    try {
+      file.url = data.replace(/^base64:\/\/.*/, "base64://...")
+      file.buffer = await this.makeBuffer(data)
+      if (Buffer.isBuffer(file.buffer)) {
+        file.type = await fileTypeFromBuffer(file.buffer)
+        file.name = `${Date.now()}.${file.type.ext}`
+      } else {
+        file.name = path.basename(file.buffer)
+      }
+    } catch (err) {
+      logger.error(`文件类型检测错误：${logger.red(err)}`)
+    }
+
+    return file
+  }
+
+  async sendMsg(data, id, msg) {
+    if (!Array.isArray(msg))
+      msg = [msg]
+    const msgs = []
+    const message_id = []
+    let quote
+    let at
+    for (let i of msg) {
+      if (typeof i != "object")
+        i = { type: "text", data: { text: i }}
+      else if (!i.data)
+        i = { type: i.type, data: { ...i, type: undefined }}
+
+      let file
+      if (i.data.file)
+        file = await this.fileType(i.data.file)
+
+      let ret
+      switch (i.type) {
+        case "text":
+          logger.info(`${logger.blue(`[${data.self_id}]`)} 发送文本：[${id}] ${i.data.text}`)
+          ret = await data.bot.sendMsg(i.data.text, id)
+          break
+        case "image":
+          logger.info(`${logger.blue(`[${data.self_id}]`)} 发送图片：[${id}] ${file.name}(${file.url})`)
+          ret = await data.bot.sendMsg({ file: file.buffer, filename: file.name }, id)
+          break
+        case "record":
+          logger.info(`${logger.blue(`[${data.self_id}]`)} 发送音频：[${id}] ${file.name}(${file.url})`)
+          ret = await data.bot.sendMsg({ file: file.buffer, filename: file.name }, id)
+          break
+        case "video":
+          logger.info(`${logger.blue(`[${data.self_id}]`)} 发送视频：[${id}] ${file.name}(${file.url})`)
+          ret = await data.bot.sendMsg({ file: file.buffer, filename: file.name }, id)
+          break
+        case "reply":
+          break
+        case "at":
+          break
+        case "node":
+          for (const ret of (await Bot.sendForwardMsg(msg => this.sendMsg(data, id, msg), i.data))) {
+            msgs.push(...ret.data)
+            message_id.push(...ret.message_id)
+          }
+          break
+        default:
+          i = JSON.stringify(i)
+          logger.info(`${logger.blue(`[${data.self_id}]`)} 发送消息：[${id}] ${i}`)
+          ret = await data.bot.sendMsg(i, id)
+      }
+      if (ret) {
+        msgs.push(ret)
+        if (ret?.MsgID)
+          message_id.push(ret.MsgID)
+      }
+    }
+    return { data: msgs, message_id }
+  }
+
+  async recallMsg(data, id, message_id) {
+    logger.info(`${logger.blue(`[${data.self_id}]`)} 撤回消息：${JSON.stringify(message_id)}`)
+    if (!Array.isArray(message_id))
+      message_id = [message_id]
+    const msgs = []
+    for (const i of message_id)
+      msgs.push(await data.bot.revokeMsg(i, id))
+    return msgs
+  }
+
+  getFriendInfo(id, user_id) {
+    const info = Bot[id].contacts[user_id]
+    if (!info) return false
+
+    return {
+      ...info,
+      user_id: `wx_${info.UserName}`,
+      nickname: info.NickName,
+      avatar: `${Bot[id].CONF.origin}${info.HeadImgUrl}`,
+    }
+  }
+
+  getFriendArray(id) {
+    const array = []
+    for (const i of Object.keys(Bot[id].contacts).filter(i => !i.startsWith("@@")))
+      array.push(this.getFriendInfo(id, i))
+    return array
+  }
+
+  getFriendList(id) {
+    const array = []
+    for (const i of (this.getFriendArray(id)))
+      array.push(i.user_id)
+    return array
+  }
+
+  getFriendMap(id) {
+    const map = new Map()
+    for (const i of (this.getFriendArray(id)))
+      map.set(i.user_id, i)
+    return map
+  }
+
+  getGroupInfo(id, group_id) {
+    const info = Bot[id].contacts[group_id]
+    if (!info) return false
+
+    return {
+      ...info,
+      group_id: `wx_${info.UserName}`,
+      group_name: info.NickName,
+      avatar: `${Bot[id].CONF.origin}${info.HeadImgUrl}`,
+    }
+  }
+
+  getGroupArray(id) {
+    const array = []
+    for (const i of Object.keys(Bot[id].contacts).filter(i => i.startsWith("@@")))
+      array.push(this.getGroupInfo(id, i))
+    return array
+  }
+
+  getGroupList(id) {
+    const array = []
+    for (const i of (this.getGroupArray(id)))
+      array.push(i.group_id)
+    return array
+  }
+
+  getGroupMap(id) {
+    const map = new Map()
+    for (const i of (this.getGroupArray(id)))
+      map.set(i.group_id, i)
+    return map
+  }
+
+  pickFriend(id, user_id) {
+    const i = {
+      ...Bot[id].fl.get(user_id),
+      self_id: id,
+      bot: Bot[id],
+      user_id: user_id.replace(/^wx_/, ""),
+    }
+    return {
+      ...i,
+      sendMsg: msg => this.sendMsg(i, i.user_id, msg),
+      recallMsg: message_id => this.recallMsg(i, i.user_id, message_id),
+      makeForwardMsg: Bot.makeForwardMsg,
+      sendForwardMsg: msg => Bot.sendForwardMsg(msg => this.sendMsg(i, i.user_id, msg), msg),
+      getAvatarUrl: () => i.avatar,
+    }
+  }
+
+  pickMember(id, group_id, user_id) {
+    const i = {
+      ...Bot[id].fl.get(user_id),
+      self_id: id,
+      bot: Bot[id],
+      group_id: group_id.replace(/^wx_/, ""),
+      user_id: user_id.replace(/^wx_/, ""),
+    }
+    return {
+      ...this.pickFriend(id, user_id),
+      ...i,
+    }
+  }
+
+  pickGroup(id, group_id) {
+    const i = {
+      ...Bot[id].gl.get(group_id),
+      self_id: id,
+      bot: Bot[id],
+      group_id: group_id.replace(/^wx_/, ""),
+    }
+    return {
+      ...i,
+      sendMsg: msg => this.sendMsg(i, i.group_id, msg),
+      recallMsg: message_id => this.recallMsg(i, i.group_id, message_id),
+      makeForwardMsg: Bot.makeForwardMsg,
+      sendForwardMsg: msg => Bot.sendForwardMsg(msg => this.sendMsg(i, i.group_id, msg), msg),
+      pickMember: user_id => this.pickMember(id, group_id, user_id),
+      getAvatarUrl: () => i.avatar,
+    }
+  }
+
+  makeMessage(data) {
+    data.post_type = "message"
+    if (data.FromUserName.startsWith("@@")) {
+      data.message_type = "group"
+      data.group_id = `wx_${data.FromUserName}`
+
+      data.content = data.Content.split(":")
+      data.raw_content = data.OriginalContent.split(":")
+
+      data.user_id = `wx_${data.raw_content.shift()}`
+      data.sender = {
+        ...data.bot.fl.get(data.user_id),
+        user_id: data.user_id,
+        nickname: data.content.shift(),
+      }
+
+      data.content = data.content.join(":").replace(/^\n/, "")
+      data.raw_content = data.raw_content.join(":").replace(/^<br\/>/, "")
+    } else {
+      data.message_type = "private"
+      data.user_id = `wx_${data.FromUserName}`
+      data.sender = data.bot.fl.get(data.user_id) || {}
+
+      data.content = data.Content
+      data.raw_content = data.OriginalContent
+    }
+    data.message_id = data.MsgId
+
+    data.message = []
+    data.raw_message = ""
+
+    switch (data.MsgType) {
+      case data.bot.CONF.MSGTYPE_TEXT:
+        data.message.push({ type: "text", text: data.content })
+        data.raw_message += data.content
+        break
+      case data.bot.CONF.MSGTYPE_IMAGE:
+        data.content = `${data.bot.CONF.API_webwxgetmsgimg}${this.makeParams({
+          MsgID: data.MsgId,
+          skey: data.bot.PROP.skey,
+          type: "big",
+        })}`
+        data.message.push({ type: "image", url: data.content })
+        data.raw_message += `[图片：${data.content}]`
+        break
+      case data.bot.CONF.MSGTYPE_VOICE:
+        data.content = `${data.bot.CONF.API_webwxgetvoice}${this.makeParams({
+          MsgID: data.MsgId,
+          skey: data.bot.PROP.skey,
+        })}`
+        data.message.push({ type: "record", url: data.content })
+        data.raw_message += `[音频：${data.content}]`
+        break
+      case data.bot.CONF.MSGTYPE_EMOTICON:
+        data.content = `${data.bot.CONF.API_webwxgetmsgimg}${this.makeParams({
+          MsgID: data.MsgId,
+          skey: data.bot.PROP.skey,
+          type: "big",
+        })}`
+        data.message.push({ type: "image", url: data.content })
+        data.raw_message += `[表情：${data.content}]`
+        break
+      case data.bot.CONF.MSGTYPE_MICROVIDEO:
+        data.content = `${data.bot.CONF.API_webwxgetvideo}${this.makeParams({
+          MsgID: data.MsgId,
+          skey: data.bot.PROP.skey,
+        })}`
+        data.message.push({ type: "video", url: data.content })
+        data.raw_message += `[小视频：${data.content}]`
+        break
+      case data.bot.CONF.MSGTYPE_VIDEO:
+        data.content = `${data.bot.CONF.API_webwxgetvideo}${this.makeParams({
+          MsgID: data.MsgId,
+          skey: data.bot.PROP.skey,
+        })}`
+        data.message.push({ type: "video", url: data.content })
+        data.raw_message += `[视频：${data.content}]`
+        break
+      case data.bot.CONF.MSGTYPE_APP:
+        switch (data.AppMsgType) {
+          case data.bot.CONF.APPMSGTYPE_ATTACH:
+            data.content = `${data.bot.CONF.API_webwxdownloadmedia}${this.makeParams({
+              sender: data.FromUserName,
+              mediaid: data.MediaId,
+              filename: data.FileName,
+              fromuser: data.bot.user.UserName,
+              pass_ticket: data.bot.PROP.passTicket,
+              webwx_data_ticket: data.bot.PROP.webwxDataTicket
+            })}`
+            data.message.push({ type: "file", url: data.content })
+            data.raw_message += `[文件：${data.content}]`
+            break
+          default:
+            data.message.push({ type: "text", text: data.content })
+            data.raw_message += data.content
+        }
+        break
+      default:
+        data.message.push({ type: "text", text: data.content })
+        data.raw_message += data.content
+    }
+
+    if (data.group_id) {
+      data.group = data.bot.pickGroup(data.group_id)
+      data.group_name = data.group.group_name
+      logger.info(`${logger.blue(`[${data.self_id}]`)} 群消息：[${data.group_name}(${data.group_id}), ${data.sender.nickname}(${data.user_id})] ${data.raw_message}`)
+      data.member = data.group.pickMember(data.user_id)
+    } else {
+      logger.info(`${logger.blue(`[${data.self_id}]`)} 好友消息：[${data.sender.nickname}(${data.user_id})] ${data.raw_message}`)
+    }
+    data.friend = data.bot.pickFriend(data.user_id)
+
+    Bot.emit(`${data.post_type}.${data.message_type}`, data)
+    Bot.emit(`${data.post_type}`, data)
+  }
+
+  async qrLogin(send) {
+    const bot = new Wechat()
+    bot.on("error", logger.error)
+    bot.on("uuid", uuid => {
+      const url = `https://login.weixin.qq.com/qrcode/${uuid}`
+      logger.mark(`微信扫码登录：${logger.green(url)}`)
+      send([`请使用微信扫码登录：${url}`, segment.image(url)])
+    })
+
+    return new Promise(resolve => {
+      bot.once("login", () => resolve(bot))
+      bot.once("logout", () => resolve(false))
+      bot.start()
+    })
+  }
+
+  async dataLogin(id) {
+    if (!fs.existsSync(`${this.path}${id}.json`))
+      return false
+
+    const bot = new Wechat(JSON.parse(fs.readFileSync(`${this.path}${id}.json`)))
+    bot.on("error", logger.error)
+
+    return new Promise(resolve => {
+      bot.once("login", () => resolve(bot))
+      bot.once("logout", () => resolve(false))
+      bot.restart()
+    })
+  }
+
+  async connect(id) {
+    let bot
+    if (typeof id == "function") {
+      bot = await this.qrLogin(id)
+    } else {
+      bot = await this.dataLogin(id)
+    }
+
+    if (!bot?.user?.Uin) {
+      logger.error(`${logger.blue(`[${id}]`)} ${this.name}(${this.id}) 连接失败`)
+      return false
+    }
+
+    id = `wx_${bot.user.Uin}`
+    fs.writeFileSync(`${this.path}${id}.json`, JSON.stringify(bot.botData))
+
+    Bot[id] = bot
+    Bot[id].info = Bot[id].user
+    Bot[id].uin = id
+    Bot[id].nickname = Bot[id].info.NickName
+    Bot[id].avatar = `${Bot[id].CONF.origin}${Bot[id].info.HeadImgUrl}`
+    Bot[id].version = {
+      id: this.id,
+      name: this.name,
+      version: config.package.dependencies.wechat4u,
+    }
+    Bot[id].stat = { start_time: Date.now()/1000 }
+
+    Bot[id].pickFriend = user_id => this.pickFriend(id, user_id)
+    Bot[id].pickUser = Bot[id].pickFriend
+
+    Bot[id].getFriendArray = () => this.getFriendArray(id)
+    Bot[id].getFriendList = () => this.getFriendList(id)
+    Bot[id].getFriendMap = () => this.getFriendMap(id)
+
+    Bot[id].pickMember = (group_id, user_id) => this.pickMember(id, group_id, user_id)
+    Bot[id].pickGroup = group_id => this.pickGroup(id, group_id)
+
+    Bot[id].getGroupArray = () => this.getGroupArray(id)
+    Bot[id].getGroupList = () => this.getGroupList(id)
+    Bot[id].getGroupMap = () => this.getGroupMap(id)
+
+    Bot[id].fl = Bot[id].getFriendMap()
+    Bot[id].gl = Bot[id].getGroupMap()
+
+    if (!config.id.includes(id)) {
+      config.id.push(id)
+      configSave(config)
+    }
+
+    if (!Bot.uin.includes(id))
+      Bot.uin.push(id)
+
+    Bot[id].on("contacts-updated", () => {
+      Bot[id].fl = Bot[id].getFriendMap()
+      Bot[id].gl = Bot[id].getGroupMap()
+    })
+
+    setTimeout(() => Bot[id].on("message", data => {
+      data.self_id = id
+      data.bot = Bot[id]
+      this.makeMessage(data)
+    }), 10000)
+
+    logger.mark(`${logger.blue(`[${id}]`)} ${this.name}(${this.id}) 已连接`)
+    Bot.emit(`connect.${id}`, Bot[id])
+    Bot.emit(`connect`, Bot[id])
+    return id
+  }
+
+  async load() {
+    for (const id of config.id)
+      await adapter.connect(id)
+    return true
+  }
+}
+
+Bot.adapter.push(adapter)
+
+export class WeChat extends plugin {
+  constructor() {
+    super({
+      name: "微信账号设置",
+      dsc: "微信账号设置",
+      event: "message",
+      rule: [
+        {
+          reg: "^#(微信|WeChat)账号$",
+          fnc: "List",
+          permission: config.permission,
+        },
+        {
+          reg: "^#(微信|WeChat)登录$",
+          fnc: "Login",
+          permission: config.permission,
+        },
+        {
+          reg: "^#(微信|WeChat)删除.+$",
+          fnc: "Remove",
+          permission: config.permission,
+        }
+      ]
+    })
+  }
+
+  async List() {
+    await this.reply(`共${config.id.length}个账号：\n${config.id.join("\n")}`, true)
+  }
+
+  async Login() {
+    if (await adapter.connect(msg => this.reply(msg, true))) {
+      await this.reply(`账号已连接，共${config.id.length}个账号`, true)
+    } else {
+      await this.reply(`账号连接失败`, true)
+      return false
+    }
+  }
+
+  async Remove() {
+    const id = this.e.msg.replace(/^#(微信|WeChat)删除/, "").trim()
+    if (!config.id.includes(id)) {
+      await this.reply(`账号不存在：${id}`, true)
+      return false
+    }
+
+    config.id = config.id.filter(item => item != id)
+    await this.reply(`账号已删除，重启后生效，共${config.id.length}个账号`, true)
+    configSave(config)
+  }
+}
+
+logger.info(logger.green("- 微信 插件 加载完成"))
